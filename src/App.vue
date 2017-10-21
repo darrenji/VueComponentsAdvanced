5f46da35b029f9f5af17145660444da488360272
darren <764190362@qq.com>
2017-10-21 10:01:32 +0800

    动态组件

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,25 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
+               <button @click="selectedComponent='appQuote'">Quote</button>
+               <button @click="selectedComponent='appAuthor'">Author</button>
+               <button @click="selectedComponent='appNew'">New</button>
+               <hr>
+               <p>{{ selectedComponent }}</p>
+               <component :is="selectedComponent">
+                   <p>Default Content</p>
+               </component>
+               
+<!--
                 <app-quote>
                     <h2 slot="title">{{ quoteTitle }}</h2>
-<!--                    <p slot="content">A wonderful Quote</p>-->
+                    <p slot="content">A wonderful Quote</p>
                     <p>A wonderful Quote</p>
                 </app-quote>
+-->
+                
+                <hr>
+                
             </div>
         </div>
     </div>
@@ -15,15 +29,19 @@
 
 <script>
     import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
     export default {
         data:function(){
             return {
                 quoteTitle: 'The Quote',
-                
+                selectedComponent: 'Quote'
             };
         },
         components: {
-            appQuote: Quote
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
         }
     }
 </script>
